ed6cc850579a4c179294e758f1dd8579c19e8301
Mehmet <101973149+kocakmehmet4@users.noreply.github.com>
2023-10-15 22:50:23 +0300

    Add files via upload

---
+++ REACT-BITIRME/src/components/Content.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../styles/Content.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "../page/Home";
+import ChangePassword from "../page/ChangePassword";
+import AddCar from "../page/AddCar";
+import EditCar from "../page/EditCar";
+import Login from "../page/auth/Login.js";
+import Registered from "../page/auth/Registered";
+import Protected from "./protected/Protected.js";
+import CarDetail from "../page/CarDetail.js";
+import MyCarList from "../page/MyCarList";
+
+function Content() {
+  return (
+    <main className="contentMain">
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/changepassword" Component={ChangePassword} />
+        <Route path="/addNewCar" Component={AddCar} />
+        <Route path="/mycar" Component={MyCarList} />
+
+        <Route path="/editCar/:carId" Component={EditCar} />
+        <Route path="/cardetail/:carId" Component={CarDetail} />
+        <Route Component={Protected}>
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Registered} />
+        </Route>
+      </Routes>
+    </main>
+  );
+}
+
+export default Content;
